97dcf1b8b76ea8810adbdad838ac18ea6a2e4f6c
b_it <57696188+danh9a99@users.noreply.github.com>
2021-06-08 11:19:50 +0700

    update tag

--- Scripts/Search.js
+++ Scripts/Search.js
@@ -1,12 +1,12 @@
 var lst_skill = [];
-
+var lst_offer = [];
+var lst_enroll = []
 $(document).ready(function(){
     q = window.location.href.split('?q=')[1]
     let query_string;
     if(q.split('%20').length != 0){
         query_string = q.replace('%20',' ');
     }
-    console.log(query_string)
     $.ajax({
         url : "http://127.0.0.1:8000/api/course-list",
         type : "POST",
@@ -19,6 +19,9 @@ $(document).ready(function(){
                 if(result.result.courses[i]._source.skill_gain != null){
                     lst_skill.push(result.result.courses[i]._source.skill_gain.split(','));
                 }
+                lst_offer.push(result.result.courses[i]._source.offer_by);
+                lst_enroll.push(result.result.courses[i]._source.enrolled.split(',')[0] + result.result.courses[i]._source.enrolled.split(',')[1])
+
                 document.getElementById('searchresult').innerHTML += '<a href="single-job-page.html" class="job-listing">' +
                 '<div class="job-listing-details">' +
                     '<div class="job-listing-company-logo">' +
@@ -38,6 +41,8 @@ $(document).ready(function(){
                 '</div>'
             }
             skill_tag()
+            offer_tag()
+            enroll_tag()
             
         }
     });
@@ -97,12 +102,63 @@ function skill_tag(){
                 '<label for="tag1">'+j.skill+'</label>'+
                 '</div>'
                 temp++;
-                console.log(j);
+        
                 
             }
           
             if(temp > 10) break;
         }
     }
-    console.log(struc)
 }
+
+function offer_tag(){
+    document.getElementById('listoffer').innerHTML  = ''
+    arr = unique(lst_offer)
+    select = document.getElementById('listoffer')
+    $('ul.dropdown-menu').attr('id', 'dropdown');
+    $('ul.dropdown-menu').attr('style','max-height: 245px; overflow-y: auto;');
+    for (var i = 0; i < arr.length; i++){
+        if(arr[i] == null){
+            continue
+        }
+        var opt = document.createElement('option');
+        opt.innerHTML = arr[i];
+        select.appendChild(opt);
+        document.getElementById('dropdown').innerHTML += '<li data-original-index="'+i+'">'+
+                    '<a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">'+
+                    '<span class="text">'+arr[i]+'</span>' +
+                    '<span class="glyphicon glyphicon-ok check-mark"></span>' +
+                    ' </a></li>'
+
+    }
+    // // With jquery
+    
+    // <li data-original-index="1">
+    //     <a tabindex="0" class="" data-tokens="null" role="option" aria-disabled="false" aria-selected="false">
+    //         <span class="text">helo</span>
+    //         <span class="glyphicon glyphicon-ok check-mark"></span>
+    //     </a>
+    // </li>
+    
+}
+function enroll_tag(){
+    console.log(lst_enroll)
+    max =  lst_enroll.sort(function(a, b){return b - a})[0]
+    min =  lst_enroll.sort(function(a, b){return a - b})[0]
+
+
+    console.log(min)
+    console.log(max)
+}
+
+
+function unique(arr) {
+    var formArr = arr.sort()
+    var newArr = [formArr[0]]
+    for (let i = 1; i < formArr.length; i++) {
+      if (formArr[i] !== formArr[i - 1]) {
+        newArr.push(formArr[i])
+      }
+    }
+    return newArr
+  }
