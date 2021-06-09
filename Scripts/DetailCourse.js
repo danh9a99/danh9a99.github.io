b0071614166dacda91097db6fe3b84ef3e708f50
b_it <57696188+danh9a99@users.noreply.github.com>
2021-06-09 17:15:36 +0700

    update some meta datas for detail page

--- Scripts/DetailCourse.js
+++ Scripts/DetailCourse.js
@@ -18,7 +18,69 @@ $(document).ready(function(){
                         '</ul>'
             document.getElementById('about').innerHTML = '<h3 class="margin-bottom-25">About this course</h3>'+
             '<p>'+result[0].about.split('SHOW')[0]+'</p>'
+            document.getElementById('meta').innerHTML = '<ul>' +
+                                '<li>' +
+                                    '<i class="icon-material-outline-speaker-notes"></i>'+
+                                    '<span>Subtitle</span>' +
+                                    '<h5>'+result[0].subtitle+'</h5>'+
+                                '</li>'+
+                                '<li>'+
+                                    '<i class="icon-material-outline-person-pin"></i>'+
+                                ' <span>Already Enrolled</span>'+
+                                ' <h5>'+result[0].enrolled+'</h5>'+
+                            ' </li>'+
+                                '<li>'+
+                                    '<i class="icon-material-outline-local-atm"></i>'+
+                                    '<span>Salary</span>'+
+                                    '<h5>$35k - $38k</h5>'+
+                                '</li>'+
+                            ' <li>'+
+                                    '<i class="icon-material-outline-access-time"></i>'+
+                                    '<span>Date Posted</span>'+
+                                    '<h5>2 days ago</h5>'+
+                                '</li>'+
+                        ' </ul>'
+            //Handling related course
+            $.ajax({
+                url:'http://127.0.0.1:8000/api/relatedcourse?keyword='+result[0].keyword,
+                type : "GET",
+                contentType:"application/json",
+                dataType:"json",
+                success : function (data){
+                    for(var i = 0; i<data.length; i ++){
+                        document.getElementById('relatedcourse').innerHTML += '<a href="#" class="job-listing">'+
 
+                    
+                                '<div class="job-listing-details">'+
+                                
+                                    '<div class="job-listing-company-logo">'+
+                                        '<img src="'+data[i].course_image+'" alt="">'+
+                                    '</div>'+
+
+                                
+                                    '<div class="job-listing-description">'+
+                                    ' <h4 class="job-listing-company">'+data[i].offer_by+'</h4>'+
+                                        '<h3 class="job-listing-title">'+data[i].course_title+'</h3>'+
+                                ' </div>'+
+                                '</div>'+
+
+                            
+                            ' <div class="job-listing-footer">'+
+                                    '<ul>'+
+                                    ' <li><i class="icon-material-outline-location-on"></i> San Francisco</li>'+
+                                        '<li><i class="icon-material-outline-business-center"></i> Full Time</li>'+
+                                        '<li><i class="icon-material-outline-account-balance-wallet"></i> $35000-$38000</li>'+
+                                        '<li><i class="icon-material-outline-access-time"></i> 2 days ago</li>'+
+                                    '</ul>'+
+                                '</div>'+
+                                '</a>'
+                            if(i == 3){
+                                break
+                            }
+                    }
+                    
+                }
+            })
         }
     });
    
